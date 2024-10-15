502a0039fb3a96919b8bbadb18610adfc948938b
21120514 <nhann9642@gmail.com>
2024-10-16 00:04:57 +0700

    github page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Photos from './components/Photos'
 import DetailPhoto from './components/DetailPhoto'
+import { Link } from 'react-router-dom'
 function App() {
 
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/photos" 
+            <Route path="/unsplash-gallery-photo/" element={
+                <>
+                <h1>Home</h1>
+                <Link to="photos">Photo page </Link>
+                </>
+            } />
+            <Route path="/unsplash-gallery-photo/photos" 
                 element={<Photos />} 
             />
-            <Route path="/photos/:id" 
+            <Route path="/unsplash-gallery-photo/photos/:id" 
                 element={<DetailPhoto />} />
         </Routes>
     </Router>
